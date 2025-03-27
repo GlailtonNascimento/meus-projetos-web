985c2dbebeaf50f6b0bdebcfda4971078021c886
Glailtonnascimento <glailtonnascimento88@gmail.com>
2025-03-27 19:24:56 -0300

    Substituído getElementById por querySelector para maior flexibilidade e melhor compatibilidade com classes CSS. Isso permite reutilizar o código para diferentes elementos no futuro.

--- meu-SitePortfolio-html/src/menu.js
+++ meu-SitePortfolio-html/src/menu.js
@@ -1,4 +1,23 @@
-btnAbrirMenu.addEventListener("click", function () {
+
+let btnMenu = document.querySelector(".btn-abrir-menu"); // Botão de abrir
+let menuMobile = document.querySelector(".menu-mobile"); // O menu
+let btnFechar = document.querySelector(".btn-fechar i"); // Ícone de fechar
+let overlay = document.querySelector(".overlay-menu"); // Overlay escuro atrás do menu
+
+// Evento para abrir o menu
+btnMenu.addEventListener("click", () => {
   menuMobile.classList.add("abrir-menu");
-  overlayMenu.style.display = "block"; // Mostra a sobreposição
+  overlay.style.display = "block"; // Exibe o overlay quando o menu abrir
+});
+
+// Evento para fechar o menu ao clicar no botão de fechar
+btnFechar.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+  overlay.style.display = "none"; // Oculta o overlay quando o menu fechar
+});
+
+// Fecha o menu ao clicar no overlay
+overlay.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+  overlay.style.display = "none";
 });
